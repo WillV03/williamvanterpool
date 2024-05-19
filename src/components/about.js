065c6706a713823baf8ec071cadb99a249e38b74
Willv03 <williamvanterpool03@gmail.com>
2024-05-19 19:30:49 -0400

    added glamour shot!

--- src/components/about.js
+++ src/components/about.js
@@ -1,12 +1,23 @@
 // import React from "react";
-
+import styled from 'styled-components';
+import '../index.css';
 
 export default function About(){
 
+const AboutContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+
     return (
-        <>
-        <h1>About</h1>
-        </>
+        <AboutContainer>
+            <h1>William Vanterpool,</h1>
+            <h1>Software Engineer</h1>
+            <img src="IMG_1755.jpg" alt="This is me" id="image"/>
+            <h1>About</h1>
+        </AboutContainer>
     )
 
 }
